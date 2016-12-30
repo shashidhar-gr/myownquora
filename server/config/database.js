b4fb0ba5a097489ee3e25d5e967f685c9d042175
shashidhar-gr <shashidhargr26@gmail.com>
2016-12-30 22:19:52 +0530

    Added mongodb URL.

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,6 @@
 //MongoDB url.
 module.exports = {
-	url : "mongodb://localhost:27017/myquora",
+	url: "mongodb://shashidharDummy:shashi123@ec2-52-91-108-184.compute-1.amazonaws.com:27017/dummyDB",
+	//url : "mongodb://localhost:27017/myquora",
 	secret : "shashidhar"
 }
